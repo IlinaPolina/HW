6d6463b7e2cf4278f51ee4680705128ebabbd57e
Polly <limpopolly@mail.ru>
2024-04-18 17:23:29 +0300

    HomeWork

---
+++ src/main/java/ru/otus/java/basic/MainApplication.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic;
+
+public class MainApplication {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello World!");
+    }
+}
