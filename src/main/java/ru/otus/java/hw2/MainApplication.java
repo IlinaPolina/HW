2ca96ad64e488ca2317a0526fff69e902eb2ecee
Polly <limpopolly@mail.ru>
2024-05-01 18:17:07 +0300

    HomeWork2

---
+++ src/main/java/ru/otus/java/hw2/MainApplication.java
@@ -0,0 +1,11 @@
+package ru.otus.java.hw2;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        int counter = 0;
+        while (counter < 5) {
+            System.out.println("A");
+            counter++;
+        }
+    }
+}
