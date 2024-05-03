5dfe2f7dbd4bb51730022ef092b3b423877a7545
Polly <limpopolly@mail.ru>
2024-05-03 13:26:47 +0300

    HomeWork2

--- src/main/java/ru/otus/java/hw2/MainApplication.java
+++ src/main/java/ru/otus/java/hw2/MainApplication.java
@@ -15,18 +15,18 @@ public class MainApplication {
             System.out.println("Печать строки i раз");
         }
         System.out.println("Задание 2");
-        sumArr();
+        sumArr(new int[]{6, 8, 9, 7, 6, 4, 3, 1, 2, 6});
         System.out.println("Задание 3");
-        arrNumbers();
+        arrNumbers(new int[]{}, 5);
         System.out.println("Задание 4");
-        arrPlus();
+        arrPlus(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
         System.out.println("Задание 5");
-        arrPastSum();
+        arrPastSum(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
 
     }
 
-    public static void sumArr() {
-        int[] arr = {6, 8, 3, 4, 5, 9, 7, 8, 1, 2};
+    public static void sumArr(int[] arr) {
+
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] >= 5) {
@@ -37,16 +37,14 @@ public class MainApplication {
 
     }
 
-    public static void arrNumbers() {
-        int[] arr = new int[5];
+    public static void arrNumbers(int[] arr, int number) {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = 7;
         }
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void arrPlus() {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public static void arrPlus(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > 0) ;
             {
@@ -57,15 +55,13 @@ public class MainApplication {
         System.out.println(Arrays.toString(arr));
     }
 
-    public static void arrPastSum() {
-        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    public static void arrPastSum(int[] arr) {
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            if (i < arr.length/2) {
+            if (i < arr.length / 2) {
                 sum += arr[i];
-            }
-            else {
-                sum +=arr [i];
+            } else {
+                sum += arr[i];
             }
         }
         System.out.println(sum + " = " + "Суммa половины с большей суммой");
