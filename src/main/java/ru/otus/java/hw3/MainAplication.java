da1ab7bf6143ad57bd3b14123a05f3b63c5afe0f
Polly <limpopolly@mail.ru>
2024-05-15 12:53:27 +0300

    Merge branch 'refs/heads/master' into lesson2

--- src/main/java/ru/otus/java/hw3/MainAplication.java
+++ src/main/java/ru/otus/java/hw3/MainAplication.java
@@ -1,10 +1,19 @@
 package ru.otus.java.hw3;
 
+import java.util.Arrays;
+
 public class MainAplication {
     public static void main(String[] args) {
         System.out.println("Задание 1");
         sumOfPositiveElements(new int[][]{{6, 8, 9, 7, 6, 4, 3, 1, 2, 6}, {9, 0, 9, 0, 6, 7, 4, 5, 6}});
+        System.out.println();
+        System.out.println("Задание 2");
+        //squareOfStars();
+        squareOfStars2(4, 4);
+        System.out.println("Задание 3");
+        zeroDiagonals();
     }
+
 
     public static int sumOfPositiveElements(int array2d[][]) {
         int sum = 0;
@@ -15,5 +24,36 @@ public class MainAplication {
             }
         }
         return sum;
+    }
+
+//    public static void squareOfStars() {
+//        char [][] array2d = new char[5][5];
+//        for (int i = 0; i < array2d.length; i++) {
+//            for (int j = 0; j < array2d[i].length; j++)
+//                array2d [i][j] = '*';
+//        }
+//        System.out.println(array2d);
+//    }
+
+    public static void squareOfStars2(int rows, int coloms) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < coloms; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void zeroDiagonals() {
+        int [][] array2d = new int [6][6];
+        for (int i = 0; i < array2d.length; i++) {
+            for (int j = 0; j < array2d[i].length; j++) {
+                if (i == j) {
+                    array2d[i][j] = 7;
+                }
+                System.out.println(array2d[i][j] + "");
+            }
+        }
+        System.out.println();
     }
 }
